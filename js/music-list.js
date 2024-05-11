1b582412ce6673bf4c5e243eb661a3ba9160ce3a
Codesphere Hosting <157127731+HostingerCodesphere@users.noreply.github.com>
2024-05-11 17:11:37 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,52 @@
+
+let allMusic = [
+
+  {
+
+    name: "No Rival - NCS",
+
+    artist: "Egzod, Maestro Chives, Alaina Cross",
+
+    img: "music-1",
+
+    src: "music-1"
+
+  },
+
+  {
+
+    name: "Let me down Slowly",
+
+    artist: "Alec Benjamin",
+
+    img: "music-2",
+
+    src: "music-2"
+
+  },
+
+  {
+
+    name: "Besabriyaan",
+
+    artist: "Armaan Malik, Amaal Mallik",
+
+    img: "music-3",
+
+    src: "music-3"
+
+  },
+
+  {
+
+    name: "Your Eyes",
+
+    artist: "Neovaii",
+
+    img: "music-4",
+
+    src: "music-4"
+
+  }
+
+];
